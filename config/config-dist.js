c7008a595108fd93e20e7f49ff53341014d3611e
Øistein Sørensen <sorenso@gmail.com>
2015-04-14 19:03:16 +0200

    Added access log file.

--- config/config-dist.js
+++ config/config-dist.js
@@ -2,6 +2,7 @@ module.exports = {
     version: '1.0.0',
     logLevel: 'debug', // debug, verbose, info
     app: {
-        port: 8000
+        port: 8000,
+        logFile: '/tmp/access.log'
     }
 };

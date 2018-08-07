e7fb6c423f70f90a1388e725ad8e5bc3ab64e2f6
Oistein Sorensen <sorenso@gmail.com>
2018-08-07 11:59:05 +0200

    Fixed small issues with the new preact component.

--- preact/my-new-widget/__tests__/src/components/hello-world/index-test.js
+++ preact/my-new-widget/__tests__/src/components/hello-world/index-test.js
@@ -6,7 +6,7 @@ import render from "preact-render-to-string";
 
 describe("src/components/HelloWorld", () => {
     it('should render HelloWorld', () => {
-        const input = render(<HelloWorld that={this} />);
-        expect(input).toBe('<div><h1>Hello, World! </h1></div>');
+        const input = render(<HelloWorld that={this} color="red" />);
+        expect(input).toBe('<div><h1 style=\"color: red h1\">Hello, World! </h1></div>');
     })
 });
--- preact/my-new-widget/src/components/hello-world/index.js
+++ preact/my-new-widget/src/components/hello-world/index.js
@@ -1,12 +1,12 @@
 import { h, Component } from 'preact';
-import './style.scss';
+import styles from './style.css';
 
 export default class App extends Component {
     render(props) {
         const that = this;
         return (
             <div>
-                <h1 style={{ color: props.color }}>Hello, World! {that.name}</h1>
+                <h1 style={`color: ${props.color} ${styles.h1}`}>Hello, World! {that.name}</h1>
             </div>
         );
     }
